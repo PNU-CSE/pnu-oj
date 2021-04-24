dcc2d7c759b3c5fcdcc33585d24bf9687e1f8f40
KyoungHwan Kim <kimkh7534@naver.com>
2021-04-24 10:19:35 +0900

    first commit

---
+++ 1001.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> a >> b;
+	if (a > b) {
+		cout << ">";
+	} else if (a < b) {
+		cout << "<";
+	} else if (a == b) {
+		cout << "==";
+	}
+	
+    return 0;
+}
+
